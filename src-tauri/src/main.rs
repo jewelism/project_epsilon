cafe1f0d12a74a5279415d7c78ef339a302dac3c
jungboseok <boseokjung@gmail.com>
2024-03-18 04:33:01 +0900

    temp: 빌드용 커밋

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,24 +12,24 @@ fn serving(name: &str) -> String {
 
 fn main() {
   tauri::Builder::default()
-    .setup(|app| {
-      tauri::WindowBuilder::new(
-        app,
-        "local", /* the unique window label */
-        tauri::WindowUrl::App("index.html".into())
-      ).build()?;
+    // .setup(|app| {
+    //   tauri::WindowBuilder::new(
+    //     app,
+    //     "local", /* the unique window label */
+    //     tauri::WindowUrl::App("index.html".into())
+    //   ).build()?;
       // tauri::WindowBuilder::new(
       //   app,
       //   "local2",
       //   tauri::WindowUrl::App("index.html".into())
       // ).build()?;
-        let window = app.get_window("main").unwrap();
-        let window2 = app.get_window("local").unwrap();
-        window.open_devtools();
-        window2.open_devtools();
+        // let window = app.get_window("main").unwrap();
+        // let window2 = app.get_window("local").unwrap();
+        // window.open_devtools();
+        // window2.open_devtools();
         // window.close_devtools();
-      Ok(())
-    })
+    //   Ok(())
+    // })
     .invoke_handler(tauri::generate_handler![serving])  
     .plugin(tauri_plugin_websocket::init())
     .run(tauri::generate_context!())
