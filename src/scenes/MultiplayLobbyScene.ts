8b2e19823268440f6bbd701ddba6976cc4b2a82c
jungboseok <boseokjung@gmail.com>
2024-03-18 15:09:58 +0900

    feat: client multi create room

--- src/scenes/MultiplayLobbyScene.ts
+++ src/scenes/MultiplayLobbyScene.ts
@@ -10,17 +10,9 @@ export class MultiplayLobbyScene extends Phaser.Scene {
   players: any[] = [];
   ipAddrInput: string = "";
   playerInfoText: HTMLInputElement;
+  roomNameInput: string;
+  inputFields: Record<string, string> = {};
 
-  constructor() {
-    super("MultiplayLobbyScene");
-  }
-  init(data) {
-    this.isHost = data.host;
-  }
-  preload() {
-    this.load.html("multiplay_lobby_form", "phaser/multiplay_lobby_form.html");
-    // this.load.image('icon', 'phaser/icon.png');
-  }
   async create() {
     this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -28,24 +20,22 @@ export class MultiplayLobbyScene extends Phaser.Scene {
     const element = this.add
       .dom(title.x, title.y + 100)
       .createFromCache("multiplay_lobby_form");
-    const ipAddrInputelement = element.getChildByName(
-      "ipAddrInput"
-    ) as HTMLInputElement;
-    ipAddrInputelement.value = "localhost";
-    this.ipAddrInput = "localhost";
-    ipAddrInputelement.addEventListener("change", (e) => {
-      this.ipAddrInput = (e.target as HTMLInputElement).value;
+
+    const setIpAddrInput = this.createInputElement(element, {
+      name: "ipAddrInput",
+      defaultValue: "localhost",
+    });
+    this.createInputElement(element, {
+      name: "roomNameInput",
+      defaultValue: "test room12",
     });
     element.getChildByID("tplink").addEventListener("click", () => {
-      const ip = "jewelry.tplinkdns.com";
-      ipAddrInputelement.value = ip;
-      this.ipAddrInput = ip;
+      setIpAddrInput("jewelry.tplinkdns.com");
     });
     element.getChildByID("localhost").addEventListener("click", () => {
-      const ip = "localhost";
-      ipAddrInputelement.value = ip;
-      this.ipAddrInput = ip;
+      setIpAddrInput("localhost");
     });
+
     element.getChildByID("connect").addEventListener("click", () => {
       this.getSocketConnection();
     });
@@ -68,13 +58,23 @@ export class MultiplayLobbyScene extends Phaser.Scene {
   }
   async getSocketConnection() {
     try {
-      this.ws = await WebSocket.connect(`ws://${this.ipAddrInput}:20058`);
+      this.playerInfoText.innerText = `try to connect...`;
+      this.ws = await WebSocket.connect(
+        `ws://${this.inputFields.ipAddrInput}:20058`
+      );
       this.playerInfoText.innerText = `waiting for players`;
     } catch (e) {
       this.playerInfoText.innerText = `ws connection failed ${e}`;
     }
     if (this.isHost) {
       this.players = [{ id: this.ws.id }];
+      this.ws.send(
+        JSON.stringify({
+          type: "room",
+          id: this.ws.id,
+          roomName: this.inputFields.roomNameInput,
+        })
+      );
     } else {
       this.ws.send(JSON.stringify({ type: "join", id: this.ws.id }));
     }
@@ -112,5 +112,30 @@ export class MultiplayLobbyScene extends Phaser.Scene {
         this.players.length
       } players: ${JSON.stringify(this.players)}`;
     });
+  }
+  createInputElement(
+    element: Phaser.GameObjects.DOMElement,
+    { name, defaultValue }
+  ) {
+    const inputEl = element.getChildByName(name) as HTMLInputElement;
+    inputEl.addEventListener("change", (e) => {
+      this.inputFields[name] = (e.target as HTMLInputElement).value;
+    });
+    const setInputValue = (value) => {
+      inputEl.value = value;
+      this.inputFields[name] = value;
+    };
+    setInputValue(defaultValue);
+    return setInputValue;
+  }
+  constructor() {
+    super("MultiplayLobbyScene");
+  }
+  init(data) {
+    this.isHost = data.host;
+  }
+  preload() {
+    this.load.html("multiplay_lobby_form", "phaser/multiplay_lobby_form.html");
+    // this.load.image('icon', 'phaser/icon.png');
   }
 }
